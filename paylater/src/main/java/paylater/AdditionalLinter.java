3ccb9895a2be3c5d4a7a5ffcde270bb4cafc7540
ramakrishnamopoori <ramakrishnamopoori@gmail.com>
2022-01-31 19:26:55 +0530

    flipcart paylater code done with maven fs

---
+++ paylater/src/main/java/paylater/AdditionalLinter.java
@@ -0,0 +1,35 @@
+package paylater;
+
+
+import schemacrawler.schema.Table;
+import schemacrawler.tools.lint.BaseLinter;
+
+public class AdditionalLinter
+  extends BaseLinter
+{
+
+  @Override
+  public String getDescription()
+  {
+    return getSummary();
+  }
+
+  @Override
+  public String getSummary()
+  {
+    return "table names should start with FOO_";
+  }
+
+  @Override
+  protected void lint(final Table table)
+  {
+    if (table != null)
+    {
+      if (!table.getName().startsWith("FOO_"))
+      {
+        addLint(table, getSummary(), table.getFullName());
+      }
+    }
+  }
+
+}
